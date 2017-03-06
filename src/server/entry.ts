7bf39799acd614eb3c06c918ec876303f69a745c
Evan Jones <evanjones4040@gmail.com>
2017-03-05 17:06:38 -0800

    port

--- src/server/entry.ts
+++ src/server/entry.ts
@@ -1,9 +1,12 @@
-
 import * as Http from 'http';
 import * as Path from 'path';
 import * as Express from 'express';
 import { getPortPromise as PortFinder } from 'portfinder';
 import Router from './router';
+
+if (process.env.NODE_ENV === 'production') {
+  process.env.PORT = 8888
+}
 
 const app = Express();
 const server = Http.createServer(app);
@@ -19,32 +22,36 @@ app.use(
 
 Router(app);
 
-const serverHasStarted = port => {
-  console.log(
-    `==> 🌎  Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`
-  );
-};
+if (process.env.PORT) {
+  server.listen(process.env.PORT);
+} else {
+  const serverHasStarted = port => {
+    console.log(
+      `==> 🌎  Listening on port ${port}. Open up http://localhost:${port}/ in your browser.`
+    );
+  };
 
-const serverHasNotStarted = err => {
-  console.log(
-    'Something went wrong while trying to start the server. Error below:'
-  );
-  console.log(err);
-};
+  const serverHasNotStarted = err => {
+    console.log(
+      'Something went wrong while trying to start the server. Error below:'
+    );
+    console.log(err);
+  };
 
-const startServer = port => {
-  return new Promise((resolve, reject) => {
-    server.listen(port, err => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(port);
-      }
+  const startServer = port => {
+    return new Promise((resolve, reject) => {
+      server.listen(port, err => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(port);
+        }
+      });
     });
-  });
-};
+  };
 
-PortFinder()
-  .then(startServer)
-  .then(serverHasStarted)
-  .catch(serverHasNotStarted);
+  PortFinder()
+    .then(startServer)
+    .then(serverHasStarted)
+    .catch(serverHasNotStarted);
+}
